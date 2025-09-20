8e4f79ea40ce75f46b4323a784359583be8ade10
Mehedi-86 <mehedihasan@mehedis-MacBook-Air.local>
2025-09-20 20:01:26 +0600

    At grade brunch

---
+++ Grade.java
@@ -0,0 +1,14 @@
+public class Grade {
+
+    public String getGrade(int score) {
+        if (score >= 90) return "A";
+        else if (score >= 80) return "B";
+        else if (score >= 70) return "C";
+        else if (score >= 60) return "D";
+        else return "F";
+    }
+
+    public boolean isPass(int score) {
+        return score >= 60;
+    }
+}
